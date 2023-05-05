f68794095ea84184a9d01244d2ab04236202de96
Michael Onyeri <omichael003@gmail.com>
2023-05-05 00:31:05 -0400

    make changes to code

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
     cpu_metric = psutil.cpu_percent()
     mem_metric = psutil.virtual_memory().percent
     Message = None
-    if cpu_metric > 82 or mem_metric > 82:
+    if cpu_metric > 80 or mem_metric > 80:
         Message = "High CPU or Memory Detected, scale up!!!"
     return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message)
 
